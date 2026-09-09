82ef0a515352707c33d02fe0da8fa38e7e3c9bd8
Moath Abdo <linktowindows00@gmail.com>
2026-09-09 17:43:10 +0300

    chore: UI enhancements on the carousel widget

--- lib/core/utils/theme/app_colors.dart
+++ lib/core/utils/theme/app_colors.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const red = Colors.red;
+  static const green = Colors.green;
+  static const black = Colors.black;
+  static const primary = Colors.blue;
 }
--- lib/features/home/view/widgets/custom_carousel_slider.dart
+++ lib/features/home/view/widgets/custom_carousel_slider.dart
@@ -1,7 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:news_app/core/utils/theme/app_colors.dart';
 import 'package:news_app/features/home/models/top_headlines_api_response.dart';
+
 
 class CustomCarouselSlider extends StatefulWidget {
   final List<Article> articles;
@@ -19,43 +22,58 @@ class _CustomCarouselSliderState extends State<CustomCarouselSlider> {
   @override
   Widget build(BuildContext context) {
     final List<Widget> imageSliders = widget.articles
-        .map((item) => Container(
-          margin: EdgeInsets.all(5.0),
-          child: ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-              child: Stack(
-                children: <Widget>[
-                  CachedNetworkImage(imageUrl: item.urlToImage?? '', fit: BoxFit.cover, width: 1000.0),
-                  Positioned(
-                    bottom: 0.0,
-                    left: 0.0,
-                    right: 0.0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(200, 0, 0, 0),
-                            Color.fromARGB(0, 0, 0, 0)
-                          ],
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                        ),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                          vertical: 10.0, horizontal: 20.0),
-                      child: Text(
-                        'No. ${widget.articles.indexOf(item)} image',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+        .map((article) {
+          final parseData = DateTime.parse(article.publishedAt ?? DateTime.now().toString());
+          final publishedDate = DateFormat.yMMMMd().format(parseData);
+          return ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+            child: Stack(
+              children: <Widget>[
+                CachedNetworkImage(imageUrl: article.urlToImage?? '', fit: BoxFit.cover, width: 1000.0),
+                Positioned(
+                  bottom: 0.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(200, 0, 0, 0),
+                          Color.fromARGB(0, 0, 0, 0)
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
                       ),
                     ),
+                    padding: EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            '${article.source?.name ??''} .$publishedDate',
+                             style: const TextStyle(
+                             color: Colors.white,
+                             fontWeight:FontWeight.bold,
+                          )
+                        ),
+                        Text(
+                          article.title ?? '',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              )),
-        ))
+                ),
+              ],
+            ));
+        })
   .toList();
 
     return Column(children: [
@@ -80,15 +98,18 @@ class _CustomCarouselSliderState extends State<CustomCarouselSlider> {
           return GestureDetector(
             onTap: () => _controller.animateToPage(entry.key),
             child: Container(
-              width: 12.0,
+              width: _current ==entry.key ? 28.0 : 12.0,
               height: 12.0,
-              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+              margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
               decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: (Theme.of(context).brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black)
-                      .withValues(alpha: _current == entry.key ? 0.9 : 0.4)),
+                borderRadius: _current ==entry.key
+                  ? const BorderRadius.all(Radius.circular(10)) : null,
+                  shape: _current ==entry.key
+                      ? BoxShape.rectangle
+                      : BoxShape.circle,
+                        color :_current ==entry.key
+                          ? AppColors.primary.withOpacity( 0.9)
+                          : AppColors.black.withOpacity(0.2)),
             ),
           );
         }).toList(),
